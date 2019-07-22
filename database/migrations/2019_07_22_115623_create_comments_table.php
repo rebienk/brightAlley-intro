6a86cd66f81c188bbcd3a6a285209422543edfd8
Burak Sen <s1127973@student.windesheim.nl>
2019-07-22 14:41:45 +0200

    added post api endpoint

--- database/migrations/2019_07_22_115623_create_comments_table.php
+++ database/migrations/2019_07_22_115623_create_comments_table.php
@@ -15,6 +15,13 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('post_id');
+            $table->string('comment_body');
+
+            $table->foreign('user_id')->references('id')->on('user')->onDelete('cascade');
+            $table->foreign('post_id')->references('id')->on('post')->onDelete('cascade');
+
             $table->timestamps();
         });
     }
